231388864ad0909c91e3d78cfcfcec36b7fc4b33
Intel <chemicstry@gmail.com>
2012-10-22 00:28:02 +0300

    Plugins/Richtext: Restore some innocent controls

--- _cms/plugins/cms_string/input_plugins/input_richtext/input_richtext.js
+++ _cms/plugins/cms_string/input_plugins/input_richtext/input_richtext.js
@@ -16,7 +16,7 @@ var Plugin_input_richtext = {
 		
 		// Load richtext editor
         string.filter("textarea").bind("showup", function() {
-            $.cleditor.defaultOptions.controls = "bold italic underline strikethrough | color highlight removeformat | bullets numbering | alignleft center | undo redo | link unlink | source";
+            $.cleditor.defaultOptions.controls = "bold italic underline strikethrough | color highlight removeformat | bullets numbering | alignleft center alignright justify | undo redo | link unlink | source";
             $(this).cleditor();
         });
 		  
