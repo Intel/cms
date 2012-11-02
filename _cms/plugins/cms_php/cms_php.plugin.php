fe01d21297eb7d43342f93eb3f0aa2401edc6207
Intel <chemicstry@gmail.com>
2012-11-02 19:00:33 +0200

    Core/Plugins: PHP Code Execution Plugin

---
+++ _cms/plugins/cms_php/cms_php.plugin.php
@@ -0,0 +1,22 @@
+<?
+    class Plugin_cms_php {
+        function On_Compiler_BuiltTemplate($a_data) {
+            // Get Template_Document from event data
+            $doc = $a_data->doc;
+            
+            // Find body container
+            $nodes = $doc->getElementsByTag('CMS_PHP');
+            
+            foreach ($nodes as $node)
+            {
+                // We do it the hard way
+                ob_start();
+                eval($node->getAttribute("code"));
+                $text = ob_get_contents();
+                ob_end_clean();
+                
+                $node->replaceWith(new Template_TextNode($text));
+            }
+        }
+    }
+?>
